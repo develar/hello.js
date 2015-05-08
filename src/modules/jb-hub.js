dec167c45a3475a49f52d49b4124b13863e73d5b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-05-08 21:48:33 +0200

    cleanup JetBrains Hub module

--- src/modules/jb-hub.js
+++ src/modules/jb-hub.js
@@ -1,50 +1,33 @@
 (function (hello) {
-  hello.init({
-    jbHub: {
-      name: "JetBrains Hub",
+    hello.init({
+        jbHub: {
+            name: "JetBrains Hub",
 
-      oauth: {
-        version: 2,
-        auth: 'https://hub-staging.labs.intellij.net/api/rest/oauth2/auth',
-        grant: 'https://hub-staging.labs.intellij.net/api/rest/oauth2/auth'
-      },
+            oauth: {
+                version: 2,
+                auth: 'https://sso.jetbrains.com/api/rest/oauth2/auth',
+                grant: 'https://sso.jetbrains.com/api/rest/oauth2/auth'
+            },
 
-      scope: {
-        basic: "0-0-0-0-0"
-      },
+            scope: {
+                basic: "0-0-0-0-0"
+            },
 
-      base: 'https://sso.jetbrains.com/api/rest/',
+            base: 'https://sso.jetbrains.com/api/rest/',
 
-      get: {
-        me: 'users/me'
-      },
-      wrap: {
-        'default': function (o, headers, req) {
-          formatError(o, headers);
-          return o;
+            get: {
+                me: 'users/me'
+            },
+
+            xhr: function (p) {
+                var accessToken = p.options.access_token
+                if (accessToken != null) {
+                    delete p.options.access_token
+                    p.headers.Authorization = "Bearer " + accessToken
+                    p.headers.Accept = "application/json"
+                }
+                return true;
+            }
         }
-      },
-
-      xhr: function(p) {
-        var accessToken = p.options.access_token
-        if (accessToken != null) {
-          delete p.options.access_token
-          p.headers.Authorization = "Bearer " + accessToken
-          p.headers.Accept = "application/json"
-        }
-        return true;
-      }
-    }
-  });
-
-  function formatError(o, headers) {
-    var code = headers ? headers.statusCode : (o && 'meta' in o && 'status' in o.meta && o.meta.status);
-    if ((code === 401 || code === 403)) {
-      o.error = {
-        code: 'access_denied',
-        message: o.message || (o.data ? o.data.message : 'Could not get response')
-      };
-      delete o.message;
-    }
-  }
-})(hello);
+    })
+})(hello)
